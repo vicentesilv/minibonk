0c9bb39ee377f42c3d6e1f1a176198f09916c71c
vicentesilv <vicentesilvala@gmail.com>
2026-04-24 22:19:04 -0700

    iniciando el proyecto

---
+++ lib/src/app/app_minibonk.dart
@@ -0,0 +1,31 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import '../game/juego_mini_bonk.dart';
+import '../ui/overlays/superposicion_hud.dart';
+import '../ui/overlays/superposicion_mejora.dart';
+
+class AppMiniBonk extends StatelessWidget {
+  const AppMiniBonk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final juego = JuegoMiniBonk();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GameWidget(
+          game: juego,
+          overlayBuilderMap: {
+            'Hud':
+                (context, juego) => SuperposicionHud(juego: juego as JuegoMiniBonk),
+            'Upgrade':
+                (context, juego) =>
+                    SuperposicionMejora(juego: juego as JuegoMiniBonk),
+          },
+        ),
+      ),
+    );
+  }
+}
